e688a23de15022d843b3455647c7803f4cf1584c
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-10-02 11:36:37 +0530

    Update aboutUs.jsx

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -33,9 +33,9 @@ const AboutUsPage = () => {
           />
           
          
-          <div className="absolute top-0 right-0 w-1/3 h-full bg-red-600" />
+          <div className="absolute top-0 right-0 w-1/3 h-full bg-red-700" />
 
-          <div className="absolute bottom-10 right-10 w-1/3 bg-white p-6 rounded-lg shadow-lg">
+         <div className="absolute bottom-10 right-20 w-1/4 bg-stone-300 p-8 rounded-lg shadow-lg">
             <p className="text-gray-800">
               Founded in 1956, Velpack has grown from a small family business into a global supplier of premium paper-based packaging. Our state-of-the-art facility in Navi Mumbai spans 42,000 sq. ft., enabling us to produce millions of cartons and inserts daily.
             </p>
