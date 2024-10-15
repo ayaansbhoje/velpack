d504fc44474063558070e83918fbb82c5a52fd06
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-10-15 09:18:59 +0530

    Update ourservices.jsx

--- src/pages/ourservices.jsx
+++ src/pages/ourservices.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Navbar from '../components/navbar';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import Navbar from '../components/navbar'
 import Footer from '../components/footer';
+
 
 const ServicesPage = () => {
   return (
