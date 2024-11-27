dd46e6f098a1b2b02f7601870c6b1e784b3d2573
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-11-27 13:03:54 +0530

    Update technology.jsx

--- src/pages/technology.jsx
+++ src/pages/technology.jsx
@@ -79,7 +79,7 @@ const Technologypage = () => {
           className="w-full h-full object-cover"
         />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <h1 className="text-white text-5xl font-hbold">OUR TECHNOLOGY</h1>
+          <h1 className="text-white text-5xl font-bold">OUR TECHNOLOGY</h1>
         </div>
       </div>
 
