ad0fc2bb323cf8d5b58d32efcf152ae8820cb21f
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-12-05 13:26:52 +0530

    Update certification.jsx

--- src/pages/certification.jsx
+++ src/pages/certification.jsx
@@ -5,6 +5,8 @@ import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 
 const CertificationPage = () => {
+
+    <Navbar />
     const certifications = [
         {
             image: "/assets/iso-9001-logo.svg",
