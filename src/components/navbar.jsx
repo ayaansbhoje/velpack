cb70e14e06ca9940bb10f3c484028a68b0560890
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-12-17 12:11:08 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -87,6 +87,13 @@ const Navbar = () => {
                 <div className="md:hidden" id="mobile-menu">
                     <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                         <Link 
+                            to="/home" 
+                            className="text-black hover:bg-gray-100 hover:text-[#e81d2d] block px-3 py-2 rounded-md text-base font-medium"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            Home
+                        </Link>
+                        <Link 
                             to="/aboutUs" 
                             className="text-black hover:bg-gray-100 hover:text-[#e81d2d] block px-3 py-2 rounded-md text-base font-medium"
                             onClick={() => setIsMenuOpen(false)}
