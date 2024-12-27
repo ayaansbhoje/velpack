866776359d4e02cb0e0c0619dce443e4f593aad4
ayaan bhoje <145797075+footbalskiller72@users.noreply.github.com>
2024-12-27 12:34:19 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 const HeroSection = () => {
     const images = [
         "/assets/HP_Banner_1.jpg",
-        "/assets/banner_2.jpg",
+        "/assets/banner_y.jpg",
         "/assets/HP_Banner_3.jpg",
         "/assets/HP_Banner_4.jpg"
     ];
