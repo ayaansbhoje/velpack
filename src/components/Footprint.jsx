d795af037409e51a27d5391046b20575f678815c
ayaan bhoje <145797075+ayaansbhoje@users.noreply.github.com>
2025-01-22 08:35:32 +0530

    Update Footprint.jsx

--- src/components/Footprint.jsx
+++ src/components/Footprint.jsx
@@ -54,7 +54,7 @@ const Footprint = () => {
   }, [isVisible]);
 
   return (
-    <div ref={sectionRef} className="relative my-10 w-full max-w-full overflow-hidden">
+    <div ref={sectionRef} className="relative my-10">
       <motion.h2
         variants={{
           hidden: { opacity: 0 },
@@ -67,11 +67,11 @@ const Footprint = () => {
         OUR FOOTPRINT
         <div className="w-16 h-1 bg-white mb-6 mx-auto mt-3"></div>
       </motion.h2>
-      <div className="relative w-full px-2">
+      <div className="relative px-2">
         <img 
           src="assets/maps_final.gif" 
           alt="World Map" 
-          className="w-[105%] sm:w-[100%] md:w-[80%] mx-auto" 
+          className="w-full md:w-[80%] mx-auto" 
         />
       </div>
     </div>
