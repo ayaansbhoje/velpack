7cdfe4ccff0b629646b1917539e4a30fca34c27a
ayaan bhoje <145797075+ayaansbhoje@users.noreply.github.com>
2025-01-23 18:24:18 +0530

    Update LogoCarousel.jsx

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LogoCarousel = ({ logos, speed = 30 }) => {
   return (
-    <div className="overflow-hidden bg-white py-4 md:py-6 flex justify-center items-center">
+    <div className="overflow-hidden bg-white py-2 md:py-6 flex justify-center items-center">
       <style jsx>{`
         @keyframes slide {
           from {
@@ -29,16 +29,18 @@ const LogoCarousel = ({ logos, speed = 30 }) => {
         {[...logos, ...logos].map((logo, index) => (
           <div 
             key={`${logo}-${index}`} 
-            className={`flex items-center justify-center mx-4 md:mx-8 ${
-              ['Bells_Healthcare', 'CVS'].includes(logo) 
-                ? 'w-56 h-28' 
-                : 'w-32 h-16'
-            }`}
+            className={`flex items-center justify-center 
+              mx-2 md:mx-4 lg:mx-8 
+              ${['Bells_Healthcare', 'CVS'].includes(logo) 
+                ? 'w-24 md:w-40 lg:w-56 h-14 md:h-24 lg:h-28' 
+                : 'w-16 md:w-24 lg:w-32 h-10 md:h-16 lg:h-16'
+              }`}
           >
             <img
               src={`/assets/${logo}.png`}
               alt={`${logo} Logo`}
-              className="max-h-full max-w-full object-contain hover:scale-110 transition-transform duration-300"
+              className="max-h-full max-w-full object-contain 
+                hover:scale-110 transition-transform duration-300"
             />
           </div>
         ))}
@@ -46,4 +48,5 @@ const LogoCarousel = ({ logos, speed = 30 }) => {
     </div>
   );
 };
+
 export default LogoCarousel;
