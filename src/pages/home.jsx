0a1a691b905afebde9fa2b51a07f9e579c11600f
ayaan bhoje <145797075+ayaansbhoje@users.noreply.github.com>
2025-01-23 16:22:31 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
         ))}
 
         {/* Overlay Content */}
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end items-center pb-12 md:pb-24">
+        <div className="absolute inset-0 bg-black bg-opacity-10 flex flex-col justify-end items-center pb-12 md:pb-24">
           <motion.h1
             variants={fadeIn("left", 1.2)}
             initial="hidden"
