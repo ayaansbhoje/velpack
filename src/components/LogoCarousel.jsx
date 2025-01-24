ee332311cabe9defbc5b929591ee53bea6cb8a3f
ayaan bhoje <145797075+ayaansbhoje@users.noreply.github.com>
2025-01-24 15:47:35 +0530

    Update LogoCarousel.jsx

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LogoCarousel = ({ logos, speed = 30 }) => {
+const LogoCarousel = ({ logos, speed = 40 }) => {
   return (
     <div className="overflow-hidden bg-white py-2 md:py-6 flex justify-center items-center">
       <style jsx>{`
