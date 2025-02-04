7ac6e5c550ad5fc4b95c0bc0ea300056e85a3150
ayaan bhoje <145797075+ayaansbhoje@users.noreply.github.com>
2025-02-04 14:37:07 +0530

    Update contactus.jsx

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -71,16 +71,19 @@ const ContactusPage = () => {
         <div className="flex flex-col md:flex-row gap-8">
           {/* Map */}
           <div className="w-full md:w-1/2">
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.616790517668!2d72.99509761491719!3d19.086388987078534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c0e936892ec7%3A0x69bc6eb3f6a36a46!2sVELPACK%20PVT.%20LTD.!5e0!3m2!1sen!2sin!4v1707026849727!5m2!1sen!2sin"
-              width="100%"
-              height="256"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="rounded-lg"
-            ></iframe>
+            <div className="w-full h-64 rounded-lg overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=19.086389,72.997280&zoom=16"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="0"
+                className="rounded-lg"
+              ></iframe>
+            </div>
           </div>
           
           {/* Contact Details */}
